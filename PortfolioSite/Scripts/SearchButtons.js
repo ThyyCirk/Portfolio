8b4a3ad44be7b8e19cd9c9cbe1dc20761a3d5ac8
ThyyLala Mac <lalapqpowow@gmail.com>
2025-05-24 21:31:56 +0200

    3rd Commit

--- PortfolioSite/Scripts/SearchButtons.js
+++ PortfolioSite/Scripts/SearchButtons.js
@@ -1,3 +1,15 @@
 function priznanjaPage() {
     window.location.href = "priznanja.html";
 }
+
+function HomePage() {
+    window.location.href = "index.html";
+}
+
+function contactPage () {
+    window.location.href = "contact.html";
+}
+
+function projektiPage () {
+    window.location.href = "projekti.html";
+}
