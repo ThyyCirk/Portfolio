3a830faf885dc3bd104731c4343e5cc362e45895
Lala <88936337+ThyyLala@users.noreply.github.com>
2026-06-28 19:05:58 +0200

    patch

--- PortfolioSite/Scripts/LinksClick.js
+++ PortfolioSite/Scripts/LinksClick.js
@@ -9,38 +9,40 @@ let show2 = document.querySelector("#msc")
 let kont2 = document.querySelector("#mkt")
 let topb = document.querySelector("#_topbar")
 
+console.log(proj, priz, show, kont, proj2, priz2, show2, kont2, topb)
+
 proj.addEventListener("click", function(){
-    window.location.href = "indexnew.html#_mp";
+    window.location.href = "index.html#_mp";
 })
 
 priz.addEventListener("click", function(){
-    window.location.href = "indexnew.html#_pz";
+    window.location.href = "index.html#_pz";
 })
 
 show.addEventListener("click", function(){
-    window.location.href = "indexnew.html#ShowcaseContainer";
+    window.location.href = "index.html#ShowcaseContainer";
 })
 
 kont.addEventListener("click", function(){
-    window.location.href = "indexnew.html#_kt";
+    window.location.href = "index.html#_kt";
 })
 
 proj2.addEventListener("click", function(){
-    window.location.href = "indexnew.html#_mp";
+    window.location.href = "index.html#_mp";
 })
 
 priz2.addEventListener("click", function(){
-    window.location.href = "indexnew.html#_pz";
+    window.location.href = "index.html#_pz";
 })
 
 show2.addEventListener("click", function(){
-    window.location.href = "indexnew.html#ShowcaseContainer";
+    window.location.href = "index.html#ShowcaseContainer";
 })
 
 kont2.addEventListener("click", function(){
-    window.location.href = "indexnew.html#_kt";
+    window.location.href = "index.html#_kt";
 })
 
 topb.addEventListener("click", function(){
-    window.location.href = "indexnew.html#TopDiv";
+    window.location.href = "index.html#TopDiv";
 })
--- PortfolioSite/Scripts/SetLang.js
+++ PortfolioSite/Scripts/SetLang.js
@@ -2,7 +2,7 @@ const setlang = document.querySelector("#setlang");
 
 setlang.addEventListener("click", function(){
     if (window.location.pathname.includes("indexnewEng.html"))
-        window.location.href = "indexnew.html"
+        window.location.href = "index.html"
     else
         window.location.href = "indexnewEng.html"
 })
